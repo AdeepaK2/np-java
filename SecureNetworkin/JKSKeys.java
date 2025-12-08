ca5a61d4dfe010ccdeace4524196d538cabeab53
AdeepaK2 <adeepashashintha@gmail.com>
2025-12-08 22:31:09 +0530

    Add JKS keystore file and implement key loading in JKSKeys class

--- SecureNetworkin/JKSKeys.java
+++ SecureNetworkin/JKSKeys.java
@@ -1,3 +1,12 @@
+import java.io.FileInputStream;
+import java.security.KeyStore;
+
 public class JKSKeys {
-  
+  public static void main(String[] args) throws Exception {
+    KeyStore keyStore = KeyStore.getInstance("JKS");
+    try (FileInputStream fis = new FileInputStream("keystore.jks")) {
+    keyStore.load(fis, "changeit".toCharArray());
+}
+
+  }
 }
