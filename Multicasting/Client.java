a550ef478f23cbe282b99c1dbd1a76c863eaf36a
AdeepaK2 <adeepashashintha@gmail.com>
2025-12-23 14:30:09 +0530

    Refactor Client class to simplify multicast socket handling and remove unused code

--- Multicasting/Client.java
+++ Multicasting/Client.java
@@ -9,16 +9,9 @@ public class Client {
   private static final String MCAST_GROUP_IP = "230.0.0.0";
   private static final int MCAST_PORT = 4446;
   public static void main(String[] args) throws SocketException,IOException {
-    MulticastSocket clientSocket=new MulticastSocket(MCAST_PORT);
-    clientSocket.joinGroup(InetAddress.getByName(MCAST_GROUP_IP));
-    byte[]  buffData=new byte[1024];
-    DatagramPacket packetIn=new DatagramPacket(buffData, buffData.length);
+    MulticastSocket socket=new MulticastSocket(MCAST_PORT);
+    socket.joinGroup(MCAST_GROUP_IP);
+    
 
-    clientSocket.receive(packetIn);
-    String receivedData=new String(packetIn.getData(),0,packetIn.getLength());
-    System.out.println("Received data: " + receivedData);
-
-    clientSocket.leaveGroup(InetAddress.getByName(MCAST_GROUP_IP));
-    clientSocket.close();
   }
 }
